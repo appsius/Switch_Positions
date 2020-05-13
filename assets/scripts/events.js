faa96d1371b917a6a3c5bb436cfb57653d1c390a
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-13 15:32:35 +0300

    Event Handler Functions && 'this' Keyword

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -40,9 +40,11 @@ div.addEventListener('mouseenter', () => {
 	console.log(event);
 });
 
-button.addEventListener('click', (event) => {
+button.addEventListener('click', function (event) {
+	event.stopPropagation();
 	console.log('BUTTON CLICKED!');
 	console.log(event);
+	console.log(this);
 });
 
 const listItems = document.querySelectorAll('li');
@@ -54,10 +56,11 @@ const list = document.querySelector('ul');
 // 	});
 // });
 
-list.addEventListener('click', (event) => {
+list.addEventListener('click', function (event) {
 	// console.log(event.currentTarget);
 	// event.target.classList.toggle('highlight');
 	event.target.closest('li').classList.toggle('highlight');
 	// form.submit();
 	button.click();
+	console.log(this);
 });

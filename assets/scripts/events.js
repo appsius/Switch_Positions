75127832f0e54fd2e061fc695f5856bfc19b9a04
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-13 14:39:48 +0300

    Working with PreventDefault()

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -27,3 +27,9 @@ buttons.forEach((btn) => {
 window.addEventListener('scroll', (event) => {
 	console.log(event);
 });
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	console.log(event);
+});

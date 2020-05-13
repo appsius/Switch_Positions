06816eac4764cb98e20e045c285f6e8c9f4d64b8
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-13 11:51:44 +0300

    Setting timeout && Setting intervals

---
+++ assets/scripts/events.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+
+// const buttonClickHandler = () => {
+// 	alert('Button Clicked!');
+// };
+
+// const anotherButtonClickedHandler = () => {
+// 	console.log('Button clicked another time...');
+// };
+
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickedHandler;
+
+button.addEventListener();
+// button.removeEventListener()

0e6458ee11635ae98d31b7dc3bbb6035e2d75b6e
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-17 10:33:33 +0300

    Module Scope && globalThis

--- assets/scripts/App/ProjectList.js
+++ assets/scripts/App/ProjectList.js
@@ -1,6 +1,8 @@
 import { ProjectItem } from './ProjectItem.js';
 import * as DOMH from '../Utility/DOMHelper.js';
 
+// console.log(DEFAULT_VALUE);
+
 export class ProjectList {
 	projects = [];
 
@@ -20,6 +22,7 @@ export class ProjectList {
 	}
 
 	connectDroppable() {
+		console.log(globalThis);
 		const list = document.querySelector(`#${this.type}-projects ul`);
 
 		list.addEventListener('dragenter', (event) => {
